733ae6b3a71e10c4bd2b524326589b7fcc63b25e
Muneer Iqbal <128808006+MSMuneerIqbal@users.noreply.github.com>
2025-01-27 05:28:21 +0500

    Update app.py

--- donation_prediction_timeseries-SMA/app.py
+++ donation_prediction_timeseries-SMA/app.py
@@ -4,7 +4,7 @@ import streamlit as st
 import matplotlib.pyplot as plt
 
 # Load your dataset
-df = pd.read_csv('./donations_data.csv')
+df = pd.read_csv('donations_data.csv')
 
 # Set the index to 'Members Name' for easier access
 df.set_index('Members Name', inplace=True)
